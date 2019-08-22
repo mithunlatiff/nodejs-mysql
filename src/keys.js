21946ead53ca964802dbcb31b8c1fef69bfa5aa2
Latiff, Mithun <Mithun.Latiff@sicpa.com>
2019-08-23 00:31:19 +0800

    ....

--- src/keys.js
+++ src/keys.js
@@ -2,10 +2,10 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: 'Password1',
-        database: 'db_links'
+        host: process.env.MYSQL_PORT_3306_TCP_ADDR,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE
     }
 
 };
